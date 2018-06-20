1931e91324f6094d26a4109de2acf371f9f6a628
angelbello87 <angelbello87@yahoo.com>
2018-06-20 16:37:41 -0600

    solved
    Problem Solved

---
+++ CowMultiplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CowMultiplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] read = Console.ReadLine().Split();
+            string a = read[0];
+            string b = read[1];
+            int c = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    c += int.Parse(a[i].ToString()) * int.Parse(b[j].ToString());
+                }
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
